f4e43121228c76b607df92ad6c448bdb016f53d4
arodriguez2018 <artrod84@gmail.com>
2022-12-06 09:22:55 -0600

    add fct

--- transform/dbt/capstone/models/staging/airbyte_schema/stg_raw_data.sql
+++ transform/dbt/capstone/models/staging/airbyte_schema/stg_raw_data.sql
@@ -1,3 +1,9 @@
 select 
-    client_day
+    client_day::DATE as DATE,
+    GAS_PRODUCTION::NUMERIC as GAS_PROD,
+    route_name,
+    stop_name
 from {{ source('airbyte_schema', 'well_values')}}
+where 
+    route_name like ('UT%') and 
+    client_day::DATE > '2022-01-01'
